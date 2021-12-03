5ce78b5f9c9a20f5fd80e69132050d7738f581c2
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2021-12-02 23:48:36 -0800

    all passing

--- src/components/header.js
+++ src/components/header.js
@@ -35,15 +35,15 @@ const Header = (title, date, temp) => {
   return headEl;
 };
 
+// TASK 2
+// ---------------------
+// Implement this function taking a css selector as its only argument.
+// It should create a header using the Header component above, passing arguments of your choosing.
+// It should append the header to the element in the DOM that matches the given selector.
+//
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
   const headerElement = Header("Bloomtech Times", "January 6, 2021", "28°");
-  document.querySelector(selector);
+  document.querySelector(selector).appendChild(headerElement);
   return headerElement;
 };
 
